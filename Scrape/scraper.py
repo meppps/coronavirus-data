bb4792255ea43107f81ed77d3b858d8bb2774578
Mikey Epps <55078570+meppps@users.noreply.github.com>
2020-04-01 21:27:25 -0700

    Scrape to JSON

--- Scrape/scraper.py
+++ Scrape/scraper.py
@@ -4,21 +4,24 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 import requests
-
-url = 'https://www.worldometers.info/coronavirus/'
-
 import requests
 
-# pretend to be a browser to avoid 403
-header = {
-  "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
-  "X-Requested-With": "XMLHttpRequest"
-}
+def scrape():
+  url = 'https://www.worldometers.info/coronavirus/'
 
-r = requests.get(url, headers=header)
+  # pretend to be a browser to avoid 403
+  header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest"
+  }
 
-dfs = pd.read_html(r.text)
+  r = requests.get(url, headers=header)
 
-df = dfs[0]
+  dfs = pd.read_html(r.text)
 
-print(df)
+  df = dfs[0]
+  print(df)
+
+  df.to_json('countries.json')
+
+scrape()
