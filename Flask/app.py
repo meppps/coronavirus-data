6e3590c74154c3e9ecc4f17da2620a4e264a6bf5
Mikey Epps <55078570+meppps@users.noreply.github.com>
2020-04-01 21:27:44 -0700

    Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,13 @@
 
-from flask import Flask
+## Initialize flask app ##
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return Welcome
+    return 'Welcome'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
